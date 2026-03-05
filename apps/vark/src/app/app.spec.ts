c4380f327e7d470a6dd3efb562ced7f1d20147f9
nouinou <nouinouhamza@gmail.com>
2026-03-05 01:20:50 +0000

    chore: remove nx welcome

--- apps/vark/src/app/app.spec.ts
+++ apps/vark/src/app/app.spec.ts
@@ -1,18 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
     }).compileComponents();
   });
 
-  it('should render title', async () => {
+  it('should render app hero heading', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome vark');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Token-backed UI');
   });
 });
