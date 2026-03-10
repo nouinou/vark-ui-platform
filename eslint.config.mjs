90ac2abce56b94a37c5a0ebece0db2f0d03ceaf5
nouinou <nouinouhamza@gmail.com>
2026-03-10 22:35:19 +0000

    chore(nx): enforce module boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,8 +17,20 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'scope:ui',
+              onlyDependOnLibsWithTags: ['scope:ui'],
+            },
+            {
+              sourceTag: 'scope:app',
+              onlyDependOnLibsWithTags: ['scope:ui', 'scope:app'],
+            },
+            {
+              sourceTag: 'type:tokens',
+              onlyDependOnLibsWithTags: ['type:tokens'],
+            },
+            {
+              sourceTag: 'type:theme',
+              onlyDependOnLibsWithTags: ['type:tokens', 'type:theme'],
             },
           ],
         },
