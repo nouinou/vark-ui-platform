7f6b3d8ecc791e4cb870e49860e977f868c18414
Hamza Nouinou <nouinouhamza@gmail.com>
2026-03-29 23:30:27 +0100

    feat(vark): add in-memory ticket creation flow on homepage (#11)
    * feat(vark): simple ticket creation
    
    * e2e(vark): create ticket tests
    
    * format: affected

--- apps/vark/src/app/app.ts
+++ apps/vark/src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { getStoredTheme, setTheme, type ThemeName } from '@vark/ui-theme';
 import { ButtonComponent, LinkComponent } from '@vark/ui-components';
 
+type Ticket = {
+  id: number;
+  title: string;
+  description: string;
+};
+
 @Component({
-  imports: [RouterModule, ButtonComponent, LinkComponent],
+  imports: [FormsModule, RouterModule, ButtonComponent, LinkComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -12,9 +19,46 @@ import { ButtonComponent, LinkComponent } from '@vark/ui-components';
 export class App {
   protected title = 'vark';
   protected activeTheme: ThemeName = getStoredTheme() ?? 'system';
+  protected isCreateTicketPanelOpen = false;
+  protected draftTicketTitle = '';
+  protected draftTicketDescription = '';
+  protected tickets: Ticket[] = [];
+  private nextTicketId = 1;
 
   protected selectTheme(theme: ThemeName) {
     setTheme(theme);
     this.activeTheme = theme;
+  }
+
+  protected openCreateTicketPanel() {
+    this.isCreateTicketPanelOpen = true;
+  }
+
+  protected cancelCreateTicket() {
+    this.isCreateTicketPanelOpen = false;
+    this.resetTicketDraft();
+  }
+
+  protected confirmCreateTicket() {
+    this.tickets = [
+      ...this.tickets,
+      {
+        id: this.nextTicketId++,
+        title: this.draftTicketTitle,
+        description: this.draftTicketDescription,
+      },
+    ];
+
+    this.isCreateTicketPanelOpen = false;
+    this.resetTicketDraft();
+  }
+
+  protected trackTicketById(_index: number, ticket: Ticket) {
+    return ticket.id;
+  }
+
+  private resetTicketDraft() {
+    this.draftTicketTitle = '';
+    this.draftTicketDescription = '';
   }
 }
